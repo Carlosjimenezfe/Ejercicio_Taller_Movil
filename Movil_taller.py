84640e8c325d66e7d9a9e3dc9bc501232c2db92f
Carlos Jimenez <valuu1216@gmail.com>
2021-09-03 09:38:40 -0500

    Numero de pulsaciones

--- Movil_taller.py
+++ Movil_taller.py
@@ -14,4 +14,11 @@ t = float(input('Digite el valor de la temperatura : '))
 masa = (p*v)/(0.37*(t+460))
 print('El valor de la masa es de : ' , masa)
 
-
+"""
+2. Calcular el número de pulsaciones que una persona debe tener por
+cada 10 segundos de ejercicio, si la fórmula es:
+Num. Pulsaciones = (200 – edad) /10.
+"""
+e = int(input('Digite el valor de la edad : '))
+pulsaciones = (200 - e)/10
+print('El numero de pulsaciones es de :', pulsaciones)
