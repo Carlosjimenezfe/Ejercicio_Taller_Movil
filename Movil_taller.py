3f0068e2e72439af275c99a5810c2b277a791bbb
Carlos Jimenez <valuu1216@gmail.com>
2021-09-03 09:23:49 -0500

    Valor de la masa

--- Movil_taller.py
+++ Movil_taller.py
@@ -4,4 +4,14 @@ Created on Fri Sep  3 09:15:18 2021
 
 @author: Usuario
 """
+"""
+1. Haga un algoritmo que calcule la masa de la siguiente fórmula:
+Masa = (presión * volúmen) / (0.37 * (temperatura + 460))
+"""
+p = float(input('Digite el valor de la presion : '))
+v = float(input('Digite el valor del volumen :'))
+t = float(input('Digite el valor de la temperatura : '))
+masa = (p*v)/(0.37*(t+460))
+print('El valor de la masa es de : ' , masa)
+
 
