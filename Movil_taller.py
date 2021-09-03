384d91b058aa01225839a51f1542ca030616a99e
Carlos Jimenez <valuu1216@gmail.com>
2021-09-03 10:05:53 -0500

    Porcentaje de cada inversor

--- Movil_taller.py
+++ Movil_taller.py
@@ -22,3 +22,29 @@ Num. Pulsaciones = (200 – edad) /10.
 e = int(input('Digite el valor de la edad : '))
 pulsaciones = (200 - e)/10
 print('El numero de pulsaciones es de :', pulsaciones)
+
+"""
+3. Tres personas deciden invertir su dinero para fundar una empresa.
+Cada una de ellas invierte una cantidad distinta. Obtener el porcentaje
+que cada quien invierte con respecto a la cantidad total invertida.
+"""
+t1 = float(input("Digite la cantidad que invirtio :"))
+t2 = float(input("Digite la cantidad que invirtio :"))
+t3 = float(input("Digite la cantidad que invirtio :"))
+
+total = t1+t2+t3
+p1 = (t1 / total)*100
+p2 = (t2 / total)*100
+p3 = (t3 / total)*100
+
+print(f'EL porcentaje del primer inversor es de {p1} %')
+print(f'EL porcentaje del primer inversor es de {p2} %')
+print(f'EL porcentaje del primer inversor es de {p3} %')
+
+      
+ 
+
+
+
+
+
